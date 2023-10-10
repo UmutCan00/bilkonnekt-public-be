5378862544dcd7ee39fc4648aa0050a3bbf27630
Umut Can Bolat <umutcanbolat03@gmail.com>
2023-10-10 13:44:00 +0300

    Update allowed_origins.js

--- config/allowed_origins.js
+++ config/allowed_origins.js
@@ -2,7 +2,10 @@ const origins = [
     'http://127.0.0.1:5500',
     'http://localhost:3500',
     'http://localhost:3000',
-    'http://127.0.0.1:5173'
+    'http://127.0.0.1:5173',
+    'https://bilkonnekt-frontend-git-test-umutcan00s-projects.vercel.app',
+    'https://bilkonnekt-frontend.vercel.app',
+    'https://fine-ruby-moth-wig.cyclic.app'
   ]
   
 module.exports = origins
