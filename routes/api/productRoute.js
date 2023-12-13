68932381db528b18927ce6de2bb0cf16f9bb877a
ozguregesire <ozguregesire@gmail.com>
2023-12-13 20:31:50 +0300

    Implement getProductsByUserId api

--- routes/api/productRoute.js
+++ routes/api/productRoute.js
@@ -6,5 +6,6 @@ const authMiddleware = require('../../middleware/auth')
 router.post('/createProduct', authMiddleware, Product.createProduct) // real create
 //router.get('/getProducts', authMiddleware, Product.getProducts) // real create
 router.get('/getProducts', Product.getProducts) // for dev purposes
+router.get('/getProductsByUserId', Product.getProductsByUserId) // for dev purposes
 
 module.exports = router
